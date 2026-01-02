3c83d1d9dccbbe142ddf27cde3aa3857af8761ea
PrasannaPandugula <prasannapandugula@gmail.com>
2026-01-02 14:21:38 +0530

    subnet and security group creation

--- modules/variables.tf
+++ modules/variables.tf
@@ -8,3 +8,12 @@ variable "pub-subnet-count" {}
 variable "pub-cidr-block" {}
 variable "pub-availability-zone" {}
 variable "pub-sub-name" {}
+
+variable "pri-sub-name" {}
+variable "pri-cidr-block" {}
+variable "pri-availability-zone" {}
+variable "public-rt-name" {}
+variable "eip-name" {}
+variable "ngw-name" {}
+variable "pri-rt-name" {}
+variable "eks-sg" {}
--- modules/vpc.tf
+++ modules/vpc.tf
@@ -35,8 +35,111 @@ resource "aws_subnet" "public-subnet" {
   availability_zone = element(var.pub-availability-zone)
 
   tags = {
-    Name = "${var.pub-sub-name}-${count-index + 1}"
+    Name = "${var.pub-sub-name}-${count.index + 1}"
     Env = var.env
     "kubernetes.io/cluster/${local.name}" = "owned"
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  depends_on = [aws_vpc.vpc]
+}
+
+resource "aws_subnet" "private-subnet" {
+  count = var.pri-sub-name
+  vpc_id = aws_vpc.vpc.id
+  cidr_block= element(var.pri-cidr-block, count.index)
+  availability_zone = element(var.pri-availability-zone)
+
+  tags = {
+    Name = "${var.pri-sub-name}-${count.index + 1}"
+    Env =  var.env
+    "kubernetes.io/cluster/${local.name}" = "owned"
+    "kubernetes.io/role/elb" = "1"
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.id
+  }
+
+  tags = {
+    Name = var.public-rt-name
+    Env= var.env
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
+
+resource "aws_route_table_association" "public-rta" {
+  count = 3
+  route_table_id = aws_route_table.public-rt.id
+  subnet_id = aws_subnet.public-subnet[count.index].id
+
+  depends_on = [ aws_vpc.vpc, aws_subnet.public-subnet ]
+}
+
+resource "aws_eip" "ngw_eip" {
+  domain = "vpc"
+  tags = {
+    Name= var.eip-name
+  }
+  depends_on = [ aws_vpc.vpc ]
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw_eip.id
+  subnet_id = aws_subnet.public-subnet[0].id
+  tags = {
+    Name= var.ngw-name
+  }
+  depends_on = [ aws_eip.ngw_eip, aws_vpc.vpc ]
+}
+
+resource "aws_route_table" "pri-rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+  tags = {
+    Name= var.pri-rt-name
+    Env = var.env
+  }
+}
+
+resource "aws_route_table_association" "pri-rt-association" {
+  count = 3
+  route_table_id = aws_route_table.pri-rt.id
+  subnet_id = aws_subnet.private-subnet.id
+
+  depends_on = [ aws_vpc.vpc, aws_subnet.private-subnet ]
+}
+
+resource "aws_security_group" "eks-cluster-sg" {
+  name = var.eks-sg
+  description = "Allow 443 from jump server"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] //Specify range
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  tags = {
+    Name = var.eks-sg
   }
 }
