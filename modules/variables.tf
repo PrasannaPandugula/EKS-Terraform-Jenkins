bed372c599c9ba7e99ad862e27a1965ecb7f66be
PrasannaPandugula <prasannapandugula@gmail.com>
2026-01-05 17:19:22 +0530

    created iam roles

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,3 +1,4 @@
+#VPC variables
 variable "cluster-name" {}
 variable "cidr-block" {}
 variable "vpc-name"  {}
@@ -5,15 +6,32 @@ variable "env" {}
 
 variable "igw-name" {}
 variable "pub-subnet-count" {}
-variable "pub-cidr-block" {}
-variable "pub-availability-zone" {}
+variable "pub-cidr-block" {
+    type = list(string)
+}
+variable "pub-availability-zone" {
+    type = list(string)
+}
 variable "pub-sub-name" {}
 
 variable "pri-sub-name" {}
-variable "pri-cidr-block" {}
-variable "pri-availability-zone" {}
+variable "pri-cidr-block" {
+    type = list(sting)
+}
+variable "pri-availability-zone" {
+    type = list(sting)
+}
 variable "public-rt-name" {}
 variable "eip-name" {}
 variable "ngw-name" {}
 variable "pri-rt-name" {}
 variable "eks-sg" {}
+
+# Iam var
+variable "is_eks_role_enabled" {
+  type = bool
+}
+
+variable "is_eks_nodegroup_role_enabled" {
+  type = bool
+}
